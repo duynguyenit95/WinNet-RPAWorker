8421a0c20b32065a1d1c144f4c5f7c6cb578c8c1
Duy Nguyen <duynguyen.it95@gmail.com>
2025-08-19 14:06:00 +0700

    Add ZAccount Scrapping worker

--- Worker/Workers/ZAccountScrapping/Program.vb
+++ Worker/Workers/ZAccountScrapping/Program.vb
@@ -1,42 +1,20 @@
-﻿Imports System
-Imports MIR7Invoice.Services
-Imports RPA.Core.Models.SAP
-Imports SAP.Utilities
+﻿Imports RPA.Core
+Imports RPA.Worker.Framework
 
 Module Program
     Public Sub Main()
-        Try
-            ' Lấy SAP GUI Scripting Object
-            Dim sapGuiAuto = GetObject("SAPGUI")
-            If sapGuiAuto Is Nothing Then
-                Console.WriteLine("Không thể kết nối với SAP GUI.")
-                Return
-            End If
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Dim workerName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name
+        Dim serverOption = New ServerOption() With {
+            .WorkerName = workerName,
+            .WorkerVersion = Application.ProductVersion
+        }
+#If Not DEBUG Then
+        serverOption.ServerUrl = ConfigurationHelper.GetAppSettingsValue("ServerUrl")
+#End If
+        Dim worker = New ZAccountScrappingWorker(serverOption)
 
-            Dim sapApp = sapGuiAuto.GetScriptingEngine()
-            Dim session = sapApp.FindById("/app/con[0]/ses[0]")
-
-            If session Is Nothing Then
-                Console.WriteLine("Không tìm thấy session SAP.")
-                Return
-            End If
-
-            ' Lấy dữ liệu từ SAP
-            Dim zAccount = New ZACCOUNT("/app/con[0]/ses[0]")
-            Dim data = zAccount.SearchResult()
-
-            If data Is Nothing OrElse data.Count = 0 Then
-                Console.WriteLine("Không tìm thấy dữ liệu từ ZACCOUNT.")
-                Return
-            End If
-
-            ' Cập nhật vào DB
-            Dim service = New MIR7InvoiceServices()
-            Dim result = service.ZAccountRecord(data).ConfigureAwait(False).GetAwaiter().GetResult()
-
-            Console.WriteLine($"Kết quả cập nhật: {result}")
-        Catch ex As Exception
-            Console.WriteLine($"Lỗi: {ex.Message}")
-        End Try
+        Application.Run(New ControlPanel(Of WorkerOption)(worker))
     End Sub
 End Module
---
+++ Worker/Workers/ZAccountScrapping/ZAccountScrappingWorker.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Threading
+Imports MIR7Invoice.Services
+Imports RPA.Core
+Imports RPA.Worker.Core
+Imports SAP.Utilities
+
+Public Class ZAccountScrappingWorker
+    Inherits TimerWorker(Of WorkerOption)
+    Private _cts As CancellationTokenSource
+    Private ReadOnly _contextId = "ZACCOUNT"
+    Public Sub New(options As ServerOption)
+        MyBase.New(options)
+
+    End Sub
+    Public Overrides Function LoadWorkerDefaultOption() As WorkerOption
+        Return New WorkerOption()
+    End Function
+
+    Public Overrides Async Function PreExecute(context As WorkerExecutionContext(Of WorkerOption)) As Task(Of WorkerExecutionContext(Of WorkerOption))
+
+        _cts = New CancellationTokenSource
+
+        Try
+            Dim sapSessionId = Await context.RequestSAPSession(_contextId)
+            Dim zAccount = New ZACCOUNT(sapSessionId)
+            Dim data = zAccount.SearchResult()
+            If data Is Nothing OrElse data.Count = 0 Then
+                context.Log("Not found any data in ZACCOUNT.")
+            Else
+                context.Log($"Found {data.Count} rows in ZACCOUNT")
+                Dim service = New MIR7InvoiceServices()
+                Await service.ZAccountRecord(data)
+            End If
+            Await context.RequestCloseSAPSession(sapSessionId, _contextId)
+            context.Log("--------------------------------------------------")
+        Catch ex As Exception
+            context.Log(ex.Message & ex.StackTrace)
+        End Try
+
+
+        Return Await MyBase.PreExecute(context)
+    End Function
+
+    Public Overrides Async Function ExecutionHandler(context As WorkerExecutionContext(Of WorkerOption)) As Task(Of WorkerExecutionContext(Of WorkerOption))
+        Return Await MyBase.ExecutionHandler(context)
+    End Function
+
+    Public Overrides Sub HandleSAPError(message As String)
+        _cts.Cancel()
+        _cts.Dispose()
+        _cts = New CancellationTokenSource
+    End Sub
+
+    Public Overrides Async Function AfterExecute(context As WorkerExecutionContext(Of WorkerOption)) As Task(Of WorkerExecutionContext(Of WorkerOption))
+        Return Await MyBase.AfterExecute(context)
+    End Function
+End Class
